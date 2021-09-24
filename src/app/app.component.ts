d7bcbf61a4d10c70982edeeb732fa67306b8e070
multiflat <multiflat7@gmail.com>
2021-09-24 12:42:15 +0900

    app.component.ts에 SplashScreen, StatusBar 관련 코드 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import { Storage } from '@ionic/storage-angular';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+//import { CardsArrayNoService } from './services/cards-array-no.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    //private storage: Storage,
+    //private cardsArrayNoService: CardsArrayNoService
+  ) {
+    //this.storage.create()
+    this.initializeApp();
+  }
+  
+
+  async initializeApp() {
+    
+    //await this.cardsArrayNoService.load();
+    
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();//backgroundColorByHexString("#2dd36f"); -> 초록색
+      this.splashScreen.hide();
+    });
+  }
 }

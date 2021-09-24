75ff67403a12ec7b23f4d73837b5eaacf2feef1b
multiflat <multiflat7@gmail.com>
2021-09-24 12:07:58 +0900

    interfaces/cardsInfo.ts 추가

---
+++ src/app/interfaces/cardsInfo.ts
@@ -0,0 +1,8 @@
+export interface CardsTimePeriod {
+    timeStamp: Date,
+    threeCards: number[],
+    isCardSelected: Boolean,
+    selectedCard: number,
+    yourPeriod: number,
+    firstDay: Date
+}

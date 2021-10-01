1ec451d7bc49b152b78771a52e3f54a1c1bde9cc
multiflat <multiflat7@gmail.com>
2021-10-01 14:02:26 +0900

    home module 시작

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { CardComponent } from "./component/card/card.component";
+import { CardStackComponent } from "./component/card-stack/card-stack.component";
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    CardComponent,
+    CardStackComponent
+  ]
 })
 export class HomePageModule {}

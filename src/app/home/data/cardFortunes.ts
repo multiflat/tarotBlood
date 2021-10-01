3089003f0cc50dc82e12f86e48572ec98109c6ac
multiflat <multiflat7@gmail.com>
2021-10-01 14:04:56 +0900

    cardFortunes ts 시작

---
+++ src/app/home/data/cardFortunes.ts
@@ -0,0 +1,199 @@
+import { Card } from "../../interfaces/card";
+
+export const cardFortunes: Card[] =  [
+    {
+      id: 0,
+      name: "바보",
+      fortune : "시선은 하늘을 향해 있으며 가슴과 팔을 활짝 벌리고 있다. 그가 딛은 길은 끊겼으나 신경쓰지 않는다. 솔방을이 그려진 가방을 어깨에 걸치고 있다. 배경에는 흰 산과 흰 태양이 있고, 그의 옆에는 흰 강아지가 있다.",
+      picture: "00fool.jpg",
+    },
+    {
+      id: 1,
+      name: "마법사",
+      fortune: "우로보로스 허리띠를 찬 마법사가 하늘과 땅을 연결하는 자세를 취하고 있다. 그의 앞에는 4원소의 상징물이 놓인 책상이 있고, 테두리는 장미와 백합이 둘러싸고 있다.",
+      picture: "01magician.jpg",
+    },
+    {
+      id: 2,
+      name: "여사제",
+      fortune: "두 개의 기둥은 솔로몬의 신전 앞에 놓여있었다는 야긴과 보아스를 가리킨다. 두 기둥 사이 석류와 종려나무 장막 앞에 여사제가 앉아 있다. 가슴에는 십자가가 손에는 토라가 들려 있다.",
+      picture: "02highPriestess.jpg",
+    },
+    {
+      id: 3,
+      name: "여황제",
+      fortune: "석류가 그려진 편안한 복장을 한 여가 아름다운 자연 속에 푹신한 의자에 편안하게 앉아 있다. 12개의 육각별이 달린 왕관을 쓰고 있으며, 의자 한켠에 놓은 하트에는 여성의 기호가 그려져 있다.",
+      picture: "03empress.jpg",
+    },
+    {
+      id: 4,
+      name: "황제",
+      fortune: "돌로 만들어진 왕좌에, 홀과 보주를 쥐고 갑옷을 입고 앉아 있다.",
+      picture: "04emperor.jpg",
+    },
+    {
+      id: 5,
+      name: "교황",
+      fortune: "",
+      picture: "05Hierophant.jpeg",
+    },
+    {
+      id: 6,
+      name: "연인",
+      fortune: " ",
+      picture: "06Lovers.jpeg",
+    },
+    {
+      id: 7,
+      name: "전차",
+      fortune: " ",
+      picture: "07Chariot.jpeg",
+    },
+    {
+      id: 8,
+      name: "힘",
+      fortune: " ",
+      picture: "08Strength.jpeg",
+    },
+    {
+      id: 9,
+      name: "은둔자",
+      fortune: " ",
+      picture: "09Hermit.jpeg",
+    },
+    {
+      id: 10,
+      name: "행운의 바퀴",
+      fortune: " ",
+      picture: "10WheelOfFortune.jpeg",
+    },
+    {
+      id: 11,
+      name: "정의",
+      fortune: " ",
+      picture: "11Justice.jpeg",
+    },
+    {
+      id: 12,
+      name: "매달린 남자",
+      fortune: " ",
+      picture: "12HangedMan.jpeg",
+    },
+    {
+      id: 13,
+      name: "죽음",
+      fortune: " ",
+      picture: "13Death.jpeg",
+    },
+    {
+      id: 14,
+      name: "절제",
+      fortune: " ",
+      picture: "14Temperance.jpeg",
+    },
+    {
+      id: 15,
+      name: "악마",
+      fortune: " ",
+      picture: "15Devil.jpeg",
+    },
+    {
+      id: 16,
+      name: "탑",
+      fortune: " ",
+      picture: "16Tower.jpeg",
+    },
+    {
+      id: 17,
+      name: "별",
+      fortune: " ",
+      picture: "17Star.jpeg",
+    },
+    {
+      id: 18,
+      name: "달",
+      fortune: " ",
+      picture: "18Moon.jpeg",
+    },
+    {
+      id: 19,
+      name: "태양",
+      fortune: " ",
+      picture: "19Sun.jpeg",
+    },
+    {
+      id: 20,
+      name: "심판",
+      fortune: " ",
+      picture: "20Judgement.jpeg",
+    },
+    {
+      id: 21,
+      name: "세계",
+      fortune: " ",
+      picture: "21World.jpeg",
+    },
+    {
+      id: 22,
+      name: "컵1",
+      fortune: " ",
+      picture: "Cups01.jpeg",
+    },
+    {
+      id: 23,
+      name: "컵2",
+      fortune: " ",
+      picture: "Cups02.jpeg",
+    },
+    {
+      id: 24,
+      name: "컵3",
+      fortune: " ",
+      picture: "Cups03.jpeg",
+    },
+    {
+      id: 25,
+      name: "컵4",
+      fortune: " ",
+      picture: "Cups04.jpeg",
+    },
+    {
+      id: 26,
+      name: "컵5",
+      fortune: " ",
+      picture: "Cups05.jpeg",
+    },
+    {
+      id: 27,
+      name: "컵6",
+      fortune: " ",
+      picture: "Cups06.jpeg",
+    },
+    {
+      id: 28,
+      name: "컵7",
+      fortune: " ",
+      picture: "Cups07.jpeg",
+    },
+    {
+      id: 29,
+      name: "컵8",
+      fortune: " ",
+      picture: "Cups08.jpeg",
+    },
+    {
+      id: 30,
+      name: "컵9",
+      fortune: " ",
+      picture: "Cups09.jpeg",
+    },
+    // {
+    //   id: 4,
+    //   name: "",
+    //   fortune: " ",
+    //   picture: ".jpeg",
+    // },
+
+  ];
+
+  
